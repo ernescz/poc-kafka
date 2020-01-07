fdfc7c978e4a46afdfa63125b2eaabee3f35a7ff
Ernests <ernescz@gmail.com>
2020-01-07 03:06:23 +0200

    consumer | updated the consumer to consume from topic

--- apps/consumer/consumer.py
+++ apps/consumer/consumer.py
@@ -1,9 +1,34 @@
-# Consumes messages in Unix timestamp and converts them to current date
-
+from kafka import KafkaConsumer
+from kafka import KafkaProducer
+from json import loads
+from json import dumps
 import datetime as d
-import sys
 
-timeStamp = 1576708981
+# Kafka consumer from 'input' topic (Unix timestamp in ms)
+consumer = KafkaConsumer(
+    'input',
+    bootstrap_servers=['192.168.100.10:9092'],
+    auto_offset_reset='earliest',
+    enable_auto_commit=True,
+    group_id='input-group',
+  )
 
-isoDate = d.datetime.fromtimestamp(timeStamp)
-print(isoDate)
+# Kafka producer for 'output' topic (ISO date format)
+producer = KafkaProducer(
+    bootstrap_servers=['192.168.100.10:9092'],
+    value_serializer=lambda x: 
+    dumps(x, default=str).encode('utf-8')
+  )
+
+# Process the messages in topic:
+for message in consumer:
+  # Decode the message that comes in stream bytes, set type to int and convert to ISO:
+  message = message.value.decode('UTF-8')
+  message_int = int(message)
+  unix_seconds = int(message_int / 1000)
+  iso_date = d.datetime.fromtimestamp(unix_seconds)
+  print('Got the value of {}'.format(iso_date))
+
+  # Send the result to the 'output' topic:
+  producer.send('output', iso_date)
+
