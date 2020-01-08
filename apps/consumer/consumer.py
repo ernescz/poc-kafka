ff6e0f5c1764cdd24d8efa25f96cbc745db8d1fe
ernescz <ernescz@gmail.com>
2020-01-08 02:26:38 +0200

    consumer | updated and Daemonized the app (#4)
    * consumer | updated the consumer to consume from topic
    
    * consumer | updated and Daemonized the app

--- apps/consumer/consumer.py
+++ apps/consumer/consumer.py
@@ -1,9 +1,49 @@
-# Consumes messages in Unix timestamp and converts them to current date
-
+from kafka import KafkaConsumer
+from kafka import KafkaProducer
+from daemonize import Daemonize
+from json import loads
+from json import dumps
 import datetime as d
-import sys
+import time as t 
 
-timeStamp = 1576708981
+def UnixConverter():
+  """
+  Consumes messages from 'input', transforms to date string and
+  sends to topic 'output'
+  """
+  time_delay = 1
 
-isoDate = d.datetime.fromtimestamp(timeStamp)
-print(isoDate)
+  # Kafka consumer from 'input' topic (Unix timestamp in ms):
+  consumer = KafkaConsumer(
+      'input',
+      bootstrap_servers=['192.168.100.10:9092'],
+      auto_offset_reset='earliest',
+      enable_auto_commit=True,
+      group_id='input-group',
+    )
+
+  # Kafka producer for 'output' topic:
+  producer = KafkaProducer(
+      bootstrap_servers=['192.168.100.10:9092'],
+      value_serializer=lambda x: 
+      dumps(x, default=str).encode('utf-8')
+    )
+
+  # Process the messages in topic:
+  for message in consumer:
+    # Decode the message from stream bytes, set type to int and convert to ISO:
+    message = message.value.decode('UTF-8')
+    message_int = int(message)
+    unix_seconds = int(message_int / 1000)
+    iso_date = d.datetime.fromtimestamp(unix_seconds)
+    print('Got the value of {}'.format(iso_date))
+
+    # Send the result to the 'output' topic, throtle the loop by "time_delay":
+    producer.send('output', iso_date)
+    t.sleep(time_delay)
+
+
+# Start and daemonize the app:
+pid = "./unix_consumer.pid"
+daemon = Daemonize(app="unix_consumer", pid=pid, action=UnixConverter)
+daemon.start()
