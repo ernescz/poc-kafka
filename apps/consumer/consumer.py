7f121f5987943957c2ba853f712649f1b2f7c229
Ernests <ernescz@gmail.com>
2020-01-08 02:22:20 +0200

    consumer | updated and Daemonized the app

--- apps/consumer/consumer.py
+++ apps/consumer/consumer.py
@@ -1,34 +1,49 @@
 from kafka import KafkaConsumer
 from kafka import KafkaProducer
+from daemonize import Daemonize
 from json import loads
 from json import dumps
 import datetime as d
+import time as t 
 
-# Kafka consumer from 'input' topic (Unix timestamp in ms)
-consumer = KafkaConsumer(
-    'input',
-    bootstrap_servers=['192.168.100.10:9092'],
-    auto_offset_reset='earliest',
-    enable_auto_commit=True,
-    group_id='input-group',
-  )
+def UnixConverter():
+  """
+  Consumes messages from 'input', transforms to date string and
+  sends to topic 'output'
+  """
+  time_delay = 1
 
-# Kafka producer for 'output' topic (ISO date format)
-producer = KafkaProducer(
-    bootstrap_servers=['192.168.100.10:9092'],
-    value_serializer=lambda x: 
-    dumps(x, default=str).encode('utf-8')
-  )
+  # Kafka consumer from 'input' topic (Unix timestamp in ms):
+  consumer = KafkaConsumer(
+      'input',
+      bootstrap_servers=['192.168.100.10:9092'],
+      auto_offset_reset='earliest',
+      enable_auto_commit=True,
+      group_id='input-group',
+    )
 
-# Process the messages in topic:
-for message in consumer:
-  # Decode the message that comes in stream bytes, set type to int and convert to ISO:
-  message = message.value.decode('UTF-8')
-  message_int = int(message)
-  unix_seconds = int(message_int / 1000)
-  iso_date = d.datetime.fromtimestamp(unix_seconds)
-  print('Got the value of {}'.format(iso_date))
+  # Kafka producer for 'output' topic:
+  producer = KafkaProducer(
+      bootstrap_servers=['192.168.100.10:9092'],
+      value_serializer=lambda x: 
+      dumps(x, default=str).encode('utf-8')
+    )
 
-  # Send the result to the 'output' topic:
-  producer.send('output', iso_date)
+  # Process the messages in topic:
+  for message in consumer:
+    # Decode the message from stream bytes, set type to int and convert to ISO:
+    message = message.value.decode('UTF-8')
+    message_int = int(message)
+    unix_seconds = int(message_int / 1000)
+    iso_date = d.datetime.fromtimestamp(unix_seconds)
+    print('Got the value of {}'.format(iso_date))
 
+    # Send the result to the 'output' topic, throtle the loop by "time_delay":
+    producer.send('output', iso_date)
+    t.sleep(time_delay)
+
+
+# Start and daemonize the app:
+pid = "./unix_consumer.pid"
+daemon = Daemonize(app="unix_consumer", pid=pid, action=UnixConverter)
+daemon.start()
